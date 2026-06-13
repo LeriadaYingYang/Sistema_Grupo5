3b13cc7d69f77ffa5a9c1d79134e0e3df3a025b9
Ortega Ascurra Mariano Fabrizio <N00471046@upn.pe>
2026-06-13 11:55:18 -0500

    Ver notas por módulo

--- director/control_academica/seguimiento_academico/ver_notas_modulo.py
+++ director/control_academica/seguimiento_academico/ver_notas_modulo.py
@@ -3,100 +3,50 @@ from director.utilidades import imprimir_titulo
 
 RUTA_NOTAS = "datos/notas_alumnos.json"
 
-
 def ver_notas_modulo():
-
     imprimir_titulo("VER NOTAS POR MÓDULO")
-
     notas = leer_json(RUTA_NOTAS)
-
     if not notas:
         print("No existen registros de notas.")
         return
-
-    modulos = sorted(
-        list(
-            set(
-                nota["id_modulo"]
-                for nota in notas
-                if nota["estado"] == "Activo"
-            )
-        )
-    )
-
+    modulos = sorted(list(set(nota["id_modulo"] for nota in notas if nota["estado"] == "Activo")))
     print("\nMÓDULOS DISPONIBLES")
-
     for modulo in modulos:
         print(f"Módulo ID: {modulo}")
-
     try:
-        id_modulo = int(
-            input("\nIngrese ID del módulo: ")
-        )
-
+        id_modulo = int(input("\nIngrese ID del módulo: "))
     except ValueError:
         print("Debe ingresar un número.")
         return
-
-    resultados = [
-
-        nota
-
-        for nota in notas
-
-        if (
-            nota["estado"] == "Activo"
-            and nota["id_modulo"] == id_modulo
-        )
-    ]
-
+    resultados = [nota for nota in notas
+        if (nota["estado"] == "Activo" and nota["id_modulo"] == id_modulo)]
     if len(resultados) == 0:
         print("No existen notas para ese módulo.")
         return
-
     imprimir_titulo("NOTAS DEL MÓDULO")
-
     suma = 0
-
     for registro in resultados:
-
         promedio = registro["promedio_modulo"]
-
         suma += promedio
-
         print(
             f"\nAlumno: {registro['nombre_alumno']}"
             f"\nID Unidad: {registro['id_unidad']}"
             f"\nPromedio Módulo: {promedio}"
         )
-
         print("\nDetalle:")
-
         for grupo in registro["grupos"]:
-
-            print(
-                f"\nGrupo: {grupo['nombre_grupo']}"
-            )
-
+            print(f"\nGrupo: {grupo['nombre_grupo']}")
             print(
                 f"Promedio Grupo: "
                 f"{grupo['promedio_grupo']}"
             )
-
             for actividad in grupo["actividades"]:
-
                 print(
                     f"  - {actividad['nombre_actividad']}: "
                     f"{actividad['nota']}"
                 )
-
         print("\n" + "-" * 40)
-
-    promedio_general = round(
-        suma / len(resultados),
-        2
-    )
-
+    promedio_general = round(suma / len(resultados),2)
     print(
         f"\nPROMEDIO GENERAL DEL MÓDULO: "
         f"{promedio_general}"
