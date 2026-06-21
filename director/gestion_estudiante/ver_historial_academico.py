3a3546a10146f702f7cb9bfbc727d59983cf4f70
JuanSanchez8678866 <prom22sanchezjuan.sa@gmail.com>
2026-06-20 21:43:09 -0500

    Delete director directory

--- director/gestion_estudiante/ver_historial_academico.py
+++
@@ -1,7 +0,0 @@
-from director.utilidades import imprimir_titulo, pausa
-
-def ver_historial_academico():
-    imprimir_titulo("HISTORIAL ACADÉMICO")
-    print("\nEste módulo se encuentra en construcción.")
-    print("Próximamente podrá consultar el historial de notas y promedios del alumno.")
-    pausa()
